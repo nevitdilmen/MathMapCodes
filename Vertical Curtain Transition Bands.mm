4677a1fcdda98b03b0c820ac8ced030ec9e5e760
Nevit Dilmen <nevilo@yahoo.com>
2018-03-18 10:21:04 +0300

    Create Vertical Curtain Transition Bands.mm

---
+++ Vertical Curtain Transition Bands.mm
@@ -0,0 +1,13 @@
+#Filter Vertical Curtain Transition Bands
+#Nevit Dilmen 2009
+#Select Image one and 2 in user values
+#Number of frames in settings
+
+filter vertical_curtain_transition_bands (image in1, image in2, int bands:1-25 (5) )
+band=1/(bands+1);
+if ((x+1)/2)%band+t*band > band then
+in1(xy)
+else
+in2(xy)
+end
+end
