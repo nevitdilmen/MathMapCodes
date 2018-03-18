3b1361bcf94b6e7d87d60c2c2d043cb94f4d5817
Nevit Dilmen <nevilo@yahoo.com>
2018-03-18 10:51:15 +0300

    Create Angular Transition Bands.mm

---
+++ Angular Transition Bands.mm
@@ -0,0 +1,13 @@
+#Filter Angular Transition Bands
+#Nevit Dilmen 2009
+#Select Image one and 2 in user values
+#Number of frames in settings
+
+filter angular_transition_bands (image in1, image in2, int bands: 1-30(10) )
+band=2*pi/bands;
+if a%band+t*band < band then
+in1(xy)
+else
+in2(xy)
+end
+end
